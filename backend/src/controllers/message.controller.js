019d7d01f9b1b593a16d81831dd1e1b3d9a3ec6b
adyybanik <adyybanik@outlook.com>
2025-02-04 23:28:41 +1100

    socket.io complete

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -2,6 +2,7 @@ import Message from "../models/message.models.js";
 import User from "../models/user.model.js";
 
 import cloudinary from "../lib/cloudinary.js"
+import { getReceiverSocketId, io } from "../lib/socket.js";
 
 export const getUsersForSidebar = async (req, res) => {
     try {
@@ -57,7 +58,10 @@ export const sendMessage = async (req, res) => {
 
         await newMessage.save();
 
-        // reattime functionality => socket.io
+        const receiverSocketId = getReceiverSocketId(receiverId);
+        if(receiverSocketId) {
+            io.to(receiverSocketId).emit("newMessage", newMessage);
+        }
 
         res.status(201).json(newMessage);
 
